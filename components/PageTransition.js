e55caa21819ddf19bb2dfd17231ae37eca2d810d
29Niro <29niro@gmail.com>
2024-09-10 12:04:24 +0530

    fix render error in pages

--- components/PageTransition.js
+++ components/PageTransition.js
@@ -6,21 +6,24 @@ import { usePathname } from 'next/navigation';
 const PageTransition = ({ children }) => {
   const pathname = usePathname();
   return (
-    <AnimatePresence>
-      <div key={pathname}>
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{
-            opacity: 0,
-            transition: { delay: 1, duration: 0.4, ease: 'easeInOut' },
-          }}
-          className="h-screen w-screen fixed top-0 left-0 bg-primary pointer-events-none"
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          {children}
-        </motion.div>
-      </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.4, ease: 'easeInOut' }}
+      >
+        {children}
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{
+          opacity: 0,
+          transition: { delay: 0.5, duration: 0.4, ease: 'easeInOut' }, // Adjust timing as needed
+        }}
+        className="h-screen w-screen fixed top-0 bg-primary pointer-events-none"
+      />
     </AnimatePresence>
   );
 };
